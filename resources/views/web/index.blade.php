45f8f7209c26155fa745d2f5f2d46e9bf9d18bb6
Waseem <waseem@datamysite.com>
2025-04-30 19:22:25 +0400

    og tags

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -8,19 +8,17 @@
 
 	      <img src="{{URL::to('/public')}}/rahaal-bg3.jpg" alt="" data-aos="fade-in">
 
-	      <div class="container" data-aos="zoom-out" data-aos-delay="100">
-	        <div class="row">
-	          <div class="col-lg-6">
+	        <div class="row hero-content">
+	          <div class="col-lg-12">
 	            <h2 class="text-theme2 text-shadow text-GamblerTown">Rahaal - The Explorer</h2>
 	            <p class="text-shadow text-white">
-	              I'm a <span class="text-theme2 text-bold text-italic">travel filmmaker</span> on a mission to <span class="text-theme text-bold text-italic">explore the world</span>, <br>
-	              tell raw human stories and <br>
+	              I'm a <span class="theme-badge">travel filmmaker</span> on a mission to <span class="theme-badge">explore the world</span>, <br>
+	              tell raw <span class="theme-badge2">human stories</span> and <br>
 	              uncover the unseen.
 	            </p>
 	            <a href="https://www.youtube.com/@rahaal_01" target="_blank" class="btn-get-started text-secrets text-shadow"><i class="bi bi-youtube"></i> Watch My Journey</a>
 	          </div>
 	        </div>
-	      </div>
 
 	    </section><!-- /Hero Section -->
 
